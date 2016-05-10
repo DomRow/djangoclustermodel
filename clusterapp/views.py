7ce0644a2794602f4fee610ec6ebb0658644b8d0
DomRown <domrowaned@gmail.com>
2016-05-10 13:10:31 +0100

    view function in order again

--- clusterapp/views.py
+++ clusterapp/views.py
@@ -14,67 +14,47 @@ import time
 
 def index(request):
 
-	template = loader.get_template('index.html')
-	
-	return render(request,'index.html', {})
-
-def details(request):
-
 	template = loader.get_template('details.html')
-	if request.method == 'POST':
-		form = ClusterForm(request.POST)
-		if form.is_valid():
-			return HttpResponseRedirect('cluster-results/')
-	else:
-		form =ClusterForm()	
-	return render(request,'details.html', {'form': form})	
-
+	form =ClusterForm()	
+	return render(request,'details.html', {'form': form})
 
 def pured3(request):
 	dataset=(request.POST.get('dataset'))
 	algorithm=(request.POST.get('algorithm'))
-	clust=(request.POST.get('clusters'))
 	numres=(request.POST.get('numresults'))
-	#name=(request.POST.get('nameselect'))
+	klust=int(request.POST.get('clusters'))
+	print (dataset)
+
+	if dataset=='ratings':
+	 	settype="ratings"
+	 	initrows=Movies_all.values_list('rating_count','rating','title').exclude(rating_count__gte=100).exclude(rating_count__lt=1)[:numres]
+	 	rows=[]
+	 	for i,e in enumerate(initrows):
+	 		x=(e[1]*10)
+	 		rows.append((e[0],int(x),e[2]))
+	 	
+	elif dataset=='iris':
+	 	settype="iris"
+	 	rows=Iris_all.values_list('sepal_width','petal_length','species')[:numres]
+	elif dataset=='blobs':
+	 	print blob1
+	 	settype="blobs"
+		rows=blob1
+	elif dataset=='rings':
+		settype="rings"
+		rows=rings1	
+	elif dataset=='crescents':
+		settype="crescents"
+	 	rows=crescent1
+	else: print ('Select')
 	
-	dataset=='IMDB - Ratings'
- 	settype="ratings"
- 	
- 	initrows=Movies_all.values_list('rating_count','rating','title').exclude(rating_count__gte=100).exclude(rating_count__lt=1)[:numres]
- 	
- 	rows=[]
- 	for i,e in enumerate(initrows):
- 		x=(e[1]*10)
+	clusted=k_means(rows,k=klust)
 
- 		rows.append((e[0],int(x),e[2]))
-		
-	# 	return rows
-	# return rows
-	# elif dataset=='IMDB - Actors':
-	# 	settype="actors"
-
-	# 	initrows=Actors.objects.count_avg()
-	# 	rows = []
-	# 	for i,e in enumerate(initrows):
-	# 		x=(e[0]*10)
-	# 		rows.append((int(x),e[1],e[2]))
-	
-
-	klust = int(clust)
-	clusted=k_means(rows,k=klust)		
-
-
-
-	##Context takes two variables - a dictionary mapping var names to var vals
-	##These vars are made available on scatter page
-	
 	context = RequestContext(request, {
 		'data': json.dumps(clusted),
 		'settype': json.dumps(settype), 
 		'dataset':dataset,
-		'clusters':klust,
-		#'name':name
-		
+		'clusters':klust
 		})
 	
-	return render(request, 'scatterchart.html',context) 
+	return render(request, 'scatterchart.html',context)  		
