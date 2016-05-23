86308d9f1f527270bbf930028e2510303094a48b
DomRown <domrowaned@gmail.com>
2016-05-23 16:14:00 +0100

    removed django_project folder

--- django_project/djangoApp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-	your_name = forms.CharField(label="your_name", max_length=100)
-
-class MovieForm(forms.Form):
-	movie = forms.CharField(label="movie_title", max_length=100)
-
-
